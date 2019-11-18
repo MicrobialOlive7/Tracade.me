d65b8c6c345de601d6603fc2792e2f538898be32
ochoiita <33903708+ochoiita@users.noreply.github.com>
2019-11-17 21:59:12 -0600

    CorrigiendoHabilidades

--- routes/web.php
+++ routes/web.php
@@ -85,7 +85,12 @@ Route::get('/Contacto', function (){
     Route::get('/Habilidades', 'Habilidades\HabilidadesController@index');
     Route::get('/AgregarHabilidades', 'Habilidades\HabilidadesController@indexCrear');
 
+
+    Route::post('/crear-habilidad', 'Habilidades\HabilidadesController@create');
+
+
     Route::get('/ModificarHabilidades/{hab_id}', 'Habilidades\HabilidadesController@indexModificar');
+    Route::get('borrar-habilidad/{id}', 'Habilidades\HabilidadesController@delete')->name('borrar-habilidad');
 
 /** Calendario **/
     Route::get('/Calendario', function (){
@@ -103,4 +108,3 @@ Route::get('/Contacto', function (){
     Route::post('crear-evento', 'EventoController@create')->name('crear-evento');
     Route::post('modificar-evento/{id}', 'EventoController@update')->name('modificar-evento');
     Route::post('eliminar-evento/{id}', 'EventoController@delete')->name('eliminar-evento');
-
