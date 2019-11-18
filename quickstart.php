4d5b2d054c1b8e3057e3be207a8fe95713866283
fPeruyero <fer.peruyero@gmail.com>
2019-11-18 10:49:59 -0600

    Crear Eventos API

--- quickstart.php
+++ quickstart.php
@@ -2,11 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-
+/*
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }
-
+*/
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
