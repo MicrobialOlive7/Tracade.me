0948ce81290cb38f44208501f45ff55d71518e90
MarthaRM <rdz.marthaelena@gmail.com>
2019-11-26 10:35:59 -0600

    Ruta y vista de perfil de instructor creado. Correccion de redireccionamiento al registrar alumno

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/Alumnos';
+    protected $redirectTo = 'alumnos';
 
     /**
      * Create a new controller instance.
